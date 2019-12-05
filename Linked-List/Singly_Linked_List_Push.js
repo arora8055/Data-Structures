6b3ceca2040b2ac30bbea8b32eca87e92f8c9729
Aman Arora <aroraaman0101@gmail.com>
2019-12-05 11:53:54 +0530

    Singly Linked List Push Operation

--- Linked-List/Singly_Linked_List_Push.js
+++ Linked-List/Singly_Linked_List_Push.js
@@ -27,3 +27,41 @@ class SinglyLinkedList{
         return this;
     }
 }
+
+
+
+class Node {
+    constructor(val){
+        this.age = age;
+        this.next = null;
+    }
+}
+
+class SinglyLinkedList {
+    constructor(){
+        this.head = null;
+        this.tail = null;
+        this.length = 0;
+    }
+
+    push(val){
+        var newNode = new Node(val);
+        console.log(this.tail)
+        if(!this.head){
+            this.head = newNode;
+            this.tail = this.head;
+        } else {
+            this.tail.next = newNode;
+            this.tail = newNode;
+        }
+        this.length++;
+        return this;        
+    }
+}
+
+var list = new SinglyLinkedList();
+list.push('Hello');
+list.push('Aman');
+list.push('How');
+list.push('Are');
+list.push('You?');
