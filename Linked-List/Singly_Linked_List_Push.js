9f6b464a8cf3e1c13f130231dba68a5cd81616d2
Aman Arora <aroraaman0101@gmail.com>
2019-12-05 15:12:12 +0530

    Singly Linked List Pop Operation

--- Linked-List/Singly_Linked_List_Push.js
+++ Linked-List/Singly_Linked_List_Push.js
@@ -30,34 +30,6 @@ class SinglyLinkedList{
 
 
 
-class Node {
-    constructor(val){
-        this.age = age;
-        this.next = null;
-    }
-}
-
-class SinglyLinkedList {
-    constructor(){
-        this.head = null;
-        this.tail = null;
-        this.length = 0;
-    }
-
-    push(val){
-        var newNode = new Node(val);
-        console.log(this.tail)
-        if(!this.head){
-            this.head = newNode;
-            this.tail = this.head;
-        } else {
-            this.tail.next = newNode;
-            this.tail = newNode;
-        }
-        this.length++;
-        return this;        
-    }
-}
 
 var list = new SinglyLinkedList();
 list.push('Hello');
