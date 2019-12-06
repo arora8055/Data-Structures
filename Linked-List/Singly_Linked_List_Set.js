8b457385435a8d49533312a4a814383edbe51e26
Aman Arora <aroraaman0101@gmail.com>
2019-12-06 12:02:15 +0530

    Finishes Set Method

--- Linked-List/Singly_Linked_List_Set.js
+++ Linked-List/Singly_Linked_List_Set.js
@@ -87,11 +87,12 @@ class SinglyLinkedList{
 
     }
 
-    set() {
-
-
+    set(index,val) {
+       var foundNode =  this.get(index);
+       if(foundNode){
+           foundNode.val = val;
+           return true;
+       }
+       return false;
     }
-
-    
-
 }
